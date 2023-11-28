23b1eab6e1ce0391fd689a07340a1aacc6a3aa18
ChrisShia <christos.shiakas@gmail.com>
2023-11-28 07:53:37 +0200

    Third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ChrisShia/puppy
 
 go 1.21.4
+
+require github.com/ChrisShia/dog v0.0.0-20231128054549-6ef2c85fd13e // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/ChrisShia/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
